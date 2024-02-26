0c73dc9615fa37b01e74bbe21af5f20fb238414f
Tiff <156123940+TiffRd@users.noreply.github.com>
2024-02-26 13:06:42 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
-
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (YOU.isHittingTile(CollisionDirection.Bottom)) {
+        YOU.vy = 0 - Math.sqrt(2 * (gravity * JumpHeight))
+    }
+})
+let YOU: Sprite = null
+let JumpHeight = 0
+let gravity = 0
+gravity = 500
+let moveSpeed = 100
+JumpHeight = 33
+tiles.setCurrentTilemap(tilemap`level5`)
+YOU = sprites.create(img`
+    . . . . . . . . . . . . . . 
+    . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+    . 6 1 f f 1 6 6 6 6 6 6 6 . 
+    . 6 1 1 1 1 6 6 6 6 6 6 6 . 
+    . 6 1 1 1 1 6 6 6 6 6 6 6 . 
+    . 6 d 1 1 6 6 6 6 6 6 6 6 . 
+    . 6 6 1 6 6 6 6 1 1 1 1 6 . 
+    . 6 6 1 6 6 f 6 1 f f 1 6 . 
+    . 6 6 6 6 f 6 f 6 1 1 d 6 . 
+    . 6 1 6 6 6 6 6 6 6 6 1 6 . 
+    . 6 6 6 6 6 6 6 6 6 6 1 6 . 
+    . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+    . 6 6 6 6 6 6 6 6 6 6 6 6 . 
+    . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+YOU.ay = gravity
+scene.cameraFollowSprite(YOU)
+YOU.setVelocity(100, 0)
